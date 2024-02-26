8ebe8a398fedc93e8cac386654efcc33cbe6183a
M0nadic.Ph0 <m0nadic.ph0@gmail.com>
2024-02-26 10:45:10 +0530

    fetch data

--- src/app/(protected)/lists/page.tsx
+++ src/app/(protected)/lists/page.tsx
@@ -1,9 +1,37 @@
+'use client'
+import { useQuery } from '@tanstack/react-query'
 import React from 'react'
 
+interface Todo {
+  id: number;
+  userId: number;
+  title: string;
+  completed: boolean;
+}
+
 export default function ListsPage() {
+  const { data: todos, isLoading, isSuccess } = useQuery<Todo[]>({
+    queryKey: ['todos'],
+    queryFn: () => fetch('https://jsonplaceholder.typicode.com/todos').then((resp) => resp.json()),
+  })
+
+
   return (
     <div>
-      Lists
+      <h1 className='text-4xl'>Lists</h1>
+      <h2 className='text-xl text-muted'>Lists you have created</h2>
+      {isLoading && (
+        <div>Loading</div>
+      )}
+      {!isLoading && (
+        <div className='flex flex-col'>
+          {todos?.map((todo: Todo) => (
+            <div key={todo.id}>
+              <div>{todo.title}</div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
